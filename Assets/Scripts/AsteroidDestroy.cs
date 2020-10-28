8b921e1f7f681ca889351bfce44e9de83e6f9990
Patrick Duell <patrick.duell98@gmail.com>
2020-10-28 16:29:17 +0100

    added explosion particle system

--- Assets/Scripts/AsteroidDestroy.cs
+++ Assets/Scripts/AsteroidDestroy.cs
@@ -2,28 +2,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class AsteroidDestroy : MonoBehaviour
 {
     public float Radius = 5.0F;
-    public float Power = 10.0F;
+    public int MinPower = 10;
+    public int MaxPower = 50;
+    public GameObject ExplosionVFX;
+    
     private void OnDestroy()
     {
         GameObject fracturedStone = Instantiate(Resources.Load("StoneTwoFractured", typeof(GameObject))) as GameObject;
         fracturedStone.transform.parent = transform.parent;
         fracturedStone.transform.position = transform.position;
-        
-        //explosion velocity
-        Vector3 explosionPos = transform.position;
-        //+3 because otherwise it's not in the center, dunnow why
-        explosionPos.y += 3;
-        Collider[] colliders = Physics.OverlapSphere(explosionPos, Radius);
-        foreach (Collider hit in colliders)
+
+
+        GameObject explosion = Instantiate(ExplosionVFX);
+        explosion.transform.position = transform.position;
+
+        Random random = new Random();
+
+
+        foreach (Transform t in fracturedStone.transform)
         {
-            Rigidbody rb = hit.GetComponent<Rigidbody>();
-        
+            var rb = t.GetComponent<Rigidbody>();
             if (rb != null)
-                rb.AddExplosionForce(Power, explosionPos, Radius, 3.0F);
+            {
+                rb.AddExplosionForce(random.Next(MinPower,MaxPower), fracturedStone.transform.position,Radius);
+            }
+            Destroy(t.gameObject,5f);
         }
+        
+        
+        
+        
+        
+        // //explosion velocity
+        // Vector3 explosionPos = fracturedStone.transform.position;
+        // Collider[] colliders = Physics.OverlapSphere(explosionPos, Radius);
+        // foreach (Collider hit in colliders)
+        // {
+        //     Rigidbody rb = hit.GetComponent<Rigidbody>();
+        //
+        //     if (rb != null)
+        //         rb.AddExplosionForce(Power, explosionPos, Radius, 3.0F);
+        // }
     }
 }
