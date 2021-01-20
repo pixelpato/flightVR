0b876ed6787c6c3c2525328411408c5ef34667e7
Patrick <patrick.duell@gmx.de>
2021-01-20 16:36:07 +0100

    added all controls for the spaceship in the vr, just have to connect it with the spaceship

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material selectMaterial = null;
+    public Material unSelectedMaterial = null;
+
+    private MeshRenderer meshRenderer = null;
+    private XRBaseInteractable interactable = null;
+
+    private bool shouldSetSelectedColor = false;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        interactable = GetComponent<XRBaseInteractable>();
+        interactable.onHoverExited.AddListener(ChangeMaterial);
+    }
+
+    private void OnDestroy()
+    {
+        interactable.onHoverExited.RemoveListener(ChangeMaterial);
+    }
+
+    private void ChangeMaterial(XRBaseInteractor interactor)
+    {
+        if (!shouldSetSelectedColor)
+        {
+            shouldSetSelectedColor = true;
+            meshRenderer.material = unSelectedMaterial;
+        }
+        else
+        {
+            shouldSetSelectedColor = false;
+            meshRenderer.material = selectMaterial;
+        }
+    }
+}
