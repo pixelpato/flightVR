de2c85a47c41a72efccf6742422198bab7f1e2d4
Patrick <patrick.duell@gmx.de>
2021-01-25 18:16:02 +0100

    added rotation button

--- Assets/Spaceship/Ship/NewPlayerController.cs
+++ Assets/Spaceship/Ship/NewPlayerController.cs
@@ -8,71 +8,61 @@ using UnityEngine.Serialization;
 
 public class NewPlayerController : MonoBehaviour
 {
-
-    public float Speed = 50f;
-    
-    
+    private Rigidbody rb;
+    public float SpeedMultiplier = 20f;
+    public float RotationSpeed = 10;
     public Rigidbody spaceShipRb;
-    
-    public Rigidbody JoystickRB;
     public JoystickControll JoystickControll;
-    private CharacterJoint characterJoint;
+    public GameObject SpeedTrigger; // z pos  for speed -> smth between 7.778907 and  7.978907
     
     
-    
-    
-    
-    public GameObject SpeedTrigger;
-    private ConfigurableJoint ConfigurableJoint; //Speed triggers joint for the data
-    public RotationButton RotationButton;
+    public RotationButton RotationButtonLeft;
+    public RotationButton RotationButtonRight;
     
     // Start is called before the first frame update
     void Start()
     {
-        characterJoint = JoystickRB.gameObject.GetComponent<CharacterJoint>();
-        ConfigurableJoint = SpeedTrigger.GetComponent<ConfigurableJoint>();
-        
-        
-        if(ConfigurableJoint == null)
-            Debug.Log("Character joint is null");
+        rb = GetComponent<Rigidbody>();
+        if(rb == null)
+            Debug.Log("rb is null");
     }
 
-
-
     
-    
-    void FixedUpdate()
-    {
-        Vector3 direction = new Vector3(JoystickControll.sideToSideTilt *-1, spaceShipRb.velocity.y,
-            JoystickControll.forwardBackwardTilt);
-
-        direction = Vector3.Normalize(direction);
-
-
-
-        Vector3 newMovement = direction * Speed * Time.deltaTime;
-
-
-
-
-         transform.Translate(newMovement *-1);
-    }
-    
-    
-    
-    // Update is called once per frame
     void Update()
     {
-
-
-        //Debug.Log("Jostick tilt is * " + JoystickControll.sideToSideTilt + " and " +JoystickControll.forwardBackwardTilt);
-        //Debug.Log("CharacterJoint: " + ConfigurableJoint.projectionDistance);
-        //Debug.Log("RotationButton: " + RotationButton.ButtonIsPressed);
-        
-        //rb.velocity = new Vector3(JoystickControll.sideToSideTilt, rb.velocity.y, JoystickControll.forwardBackwardTilt) * Speed;
-
-
-
+      MoveSpaceship();
+      RotateSpaceship();
 
     }
+
+    private void RotateSpaceship()
+    {
+        if (RotationButtonLeft.ButtonIsPressed && !RotationButtonRight.ButtonIsPressed)
+            transform.Rotate((new Vector3(0,-RotationSpeed,0)) *Time.deltaTime);
+        else if(!RotationButtonLeft.ButtonIsPressed && RotationButtonRight.ButtonIsPressed)
+            transform.Rotate((new Vector3(0,RotationSpeed,0)) *Time.deltaTime);
+    }
+
+
+    private void MoveSpaceship()
+    {
+        float speed = GetSpeed();
+        Vector3 direction = new Vector3(JoystickControll.sideToSideTilt *-1, spaceShipRb.velocity.y, JoystickControll.forwardBackwardTilt);
+        direction = Vector3.Normalize(direction);
+        Vector3 newMovement = direction * speed * Time.deltaTime;
+        Debug.Log("new movement is " + newMovement); 
+        transform.Translate(newMovement *-1);
+    }
+
+    private float GetSpeed() //smth between -6 and 6 i guess
+    {
+        double zPos = SpeedTrigger.transform.localPosition.z;
+        zPos = 1 - (zPos / 10);
+        double newSpeed = zPos * SpeedMultiplier;
+        Debug.Log("new speed is " + newSpeed); 
+        return (float) Math.Abs(newSpeed);
+    }
+
+
+
 }
