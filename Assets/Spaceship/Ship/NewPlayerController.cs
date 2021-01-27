ea32b7fe8dbdc7ef5a66261b7069430ea2f00b1b
Patrick <patrick.duell@gmx.de>
2021-01-27 16:35:27 +0100

    finished the speedtrigger

--- Assets/Spaceship/Ship/NewPlayerController.cs
+++ Assets/Spaceship/Ship/NewPlayerController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.PlayerLoop;
 using UnityEngine.Serialization;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NewPlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float SpeedMultiplier = 20f;
+    private float OldSpeed;
+    
     public float RotationSpeed = 10;
     public Rigidbody spaceShipRb;
     public JoystickControll JoystickControll;
-    public GameObject SpeedTrigger; // z pos  for speed -> smth between 7.778907 and  7.978907
-
+    
+    
+    public GameObject SpeedTrigger;
+    public GameObject SpeedTriggerStartPoint;
+    private XRGrabInteractable speedTriggerInteract; 
 
     public RotationButton RotationButtonLeft;
     public RotationButton RotationButtonRight;
@@ -22,6 +28,8 @@ public class NewPlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        OldSpeed = 0;
+        speedTriggerInteract = SpeedTrigger.GetComponent<XRGrabInteractable>();
         rb = GetComponent<Rigidbody>();
         if (rb == null)
             Debug.Log("rb is null");
@@ -56,13 +64,20 @@ public class NewPlayerController : MonoBehaviour
 
     private float GetSpeed() //smth between -6 and 6 i guess
     {
-        double zPos = SpeedTrigger.transform.localPosition.z;
-        zPos = 1 - (zPos / 10);
-        double newSpeed = zPos * SpeedMultiplier;
-        Debug.Log("new speed is " + newSpeed);
-        return (float)Math.Abs(newSpeed);
+        if (!speedTriggerInteract.isSelected)
+        {
+            float distance =SpeedTrigger.transform.position.z -  SpeedTriggerStartPoint.transform.position.z;
+
+            float newSpeed = distance * SpeedMultiplier; 
+            
+            
+            Debug.Log("new speed is " + newSpeed);
+            //double zPos = SpeedTrigger.transform.localPosition.z;
+            //zPos = 1 - (zPos / 10);
+            //double newSpeed = zPos * SpeedMultiplier;
+            OldSpeed = (float) Math.Abs(newSpeed);
+            return (float) Math.Abs(newSpeed);
+        }
+        return OldSpeed;
     }
-
-
-
 }
