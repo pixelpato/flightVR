f94f963a783439fe0b7aa9af5796a79219232e14
Patrick <patrick.duell@gmx.de>
2021-01-30 13:47:50 +0100

    fixed asterpid explosion

---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    public GameObject Asteroid;
+
+    public XRController RightHand ;
+    UnityEngine.XR.InputDevice device;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+
+        if(leftHandDevices.Count == 1)
+        {
+            device = leftHandDevices[0];
+            Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
+        }
+        else if(leftHandDevices.Count > 1)
+        {
+            Debug.Log("Found more than one left hand!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool triggerValue;
+        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+        {
+            Debug.Log("Trigger button is pressed.");
+            Destroy(Asteroid);
+        }
+    }
+}
